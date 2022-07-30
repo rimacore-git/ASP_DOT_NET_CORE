aafac9f926cfc4db6ac0206571a88980ef8c9de2
rimacore-git <2016rimadas@gmail.com>
2022-07-30 07:51:28 +0530

    Part11 Controller Add

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webgentle.BookStore.Controller
+{
+    public class HomeController:ControllerBase
+    {
+        public string Index()
+        {
+            return "Web Gentle";
+        }
+    }
+}
--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +26,51 @@ namespace Webgentle.BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.Use(async (context, next) =>
+            //{
 
+            //    await context.Response.WriteAsync("Hello from my first MiddleWare");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my first MiddleWare response");
+            //});
+
+
+            //app.Use(async (context, next) =>
+            //{
+
+            //    await context.Response.WriteAsync("Hello from my second MiddleWare");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my second MiddleWare response");
+
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+
+            //    await context.Response.WriteAsync("Hello from my third MiddleWare");
+            //    await next();
+
+            //});
             app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+
+            //    });
+
+            //});
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/Rima", async context =>
+                //{
+                //    await context.Response.WriteAsync(env.EnvironmentName);
+                //});
+
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
